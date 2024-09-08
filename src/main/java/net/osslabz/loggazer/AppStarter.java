b6bfa6bd783a75feb333ebf66c48aad3aa8c4c36
Raphael Vullriede <raphael@osslabz.net>
2024-09-08 20:07:45 +0200

    more build experiments

--- src/main/java/net/osslabz/loggazer/AppStarter.java
+++ src/main/java/net/osslabz/loggazer/AppStarter.java
@@ -3,6 +3,7 @@ package net.osslabz.loggazer;
 public class AppStarter {
 
     public static void main(final String[] args) {
-        LogGazerApp.main(args);
+        LogGazerApp logGazerApp = new LogGazerApp();
+        logGazerApp.main(args);
     }
 }
