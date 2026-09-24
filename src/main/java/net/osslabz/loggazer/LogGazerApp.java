eb702e47e6ab97e244e6f9fe25ea274c6880489e
Raphael <raphael@osslabz.net>
2026-09-24 23:38:26 +0200

    refactor: print the version without a format string

--- src/main/java/net/osslabz/loggazer/LogGazerApp.java
+++ src/main/java/net/osslabz/loggazer/LogGazerApp.java
@@ -78,13 +78,12 @@ public class LogGazerApp extends Application {
         }
 
         if (args != null && args.length == 1 && isVersionOption(args[0])) {
-            System.out.printf("""
-                            log-gazer %s
+            System.out.print("log-gazer " + LogGazerApp.class.getPackage().getImplementationVersion() + "\n" + """
                             Copyright (C) 2024 Raphael Vullriede (raphael@osslabz.net)
                             License: Apache License Version 2.0, January 2004 <https://www.apache.org/licenses/LICENSE-2.0.txt>.
                             This is free software: you are free to change and redistribute it.
                             There is NO WARRANTY, to the extent permitted by law.
-                            """, LogGazerApp.class.getPackage().getImplementationVersion());
+                            """);
             System.exit(0);
         }
 
